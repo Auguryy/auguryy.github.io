1dbdaa81b790bb16b11a21465f30305882c14758
Jainish Shah <sjainish31@yahoo.com>
2023-10-13 13:54:31 -0500

    Minor Changes

--- js/main.js
+++ js/main.js
@@ -8,15 +8,6 @@ for (var i = 0; i < coll.length; i++) {
     // Select the content associated with the clicked element
     var content = this.nextElementSibling;
 
-    // Close all other open sections
-    var allCollapsibles = document.getElementsByClassName("collapsible");
-    for (var j = 0; j < allCollapsibles.length; j++) {
-      if (allCollapsibles[j] !== this) {
-        allCollapsibles[j].classList.remove("active");
-        allCollapsibles[j].nextElementSibling.style.maxHeight = null;
-      }
-    }
-
     // Toggle the max height of the content to expand or collapse it
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
